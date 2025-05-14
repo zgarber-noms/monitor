c57509a8be0a3d483212d9f1e4c9c0ac4e3c333c
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-14 02:34:01 +0000

    Created Components.Autocomplete.AutoCompleteOption; Edited Components.Autocomplete; Deleted Components.Autocomplete.ItemTemplate2

--- client_code/Components/Autocomplete/AutoCompleteOption/__init__.py
+++ client_code/Components/Autocomplete/AutoCompleteOption/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate2Template
+from ._anvil_designer import AutoCompleteOptionTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,7 +9,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ItemTemplate2(ItemTemplate2Template):
+class AutoCompleteOption(AutoCompleteOptionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
