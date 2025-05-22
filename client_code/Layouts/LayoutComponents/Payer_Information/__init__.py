104cf3846a47e23dc78fa8e3798e253a8c3f6200
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-22 14:02:18 +0000

    Edited Layouts.LayoutComponents.Payer_Information

--- client_code/Layouts/LayoutComponents/Payer_Information/__init__.py
+++ client_code/Layouts/LayoutComponents/Payer_Information/__init__.py
@@ -9,5 +9,5 @@ class Payer_Information(Payer_InformationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.column_panel_1.w
+    
     # Any code you write here will run before the form opens.
