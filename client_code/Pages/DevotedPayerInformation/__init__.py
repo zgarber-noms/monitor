19a33a671efe28ce31b6823c3945bf2d599ccab1
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-27 13:19:40 +0000

    Created Pages.AnthemPayerInformation and Pages.DevotedPayerInformation

---
+++ client_code/Pages/DevotedPayerInformation/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import DevotedPayerInformationTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+class DevotedPayerInformation(DevotedPayerInformationTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
