10c353f3a09d4f01432ce33452a0c57cf034c08c
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-27 16:41:20 +0000

    Created Pages.EmptyForm; Edited settings and Pages.PayerInformation.ACOReachPayerInformation

--- client_code/Pages/EmptyForm/__init__.py
+++ client_code/Pages/EmptyForm/__init__.py
@@ -1,15 +1,13 @@
-from ._anvil_designer import ACOReachPayerInformationTemplate
+from ._anvil_designer import EmptyFormTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 
-class ACOReachPayerInformation(ACOReachPayerInformationTemplate):
+class EmptyForm(EmptyFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.item['test_var'] = 'Test'
-    self.refresh_data_bindings(())
 
     # Any code you write here will run before the form opens.
