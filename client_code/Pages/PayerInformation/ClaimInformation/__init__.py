84a2edca7191db9df7d7f8eeca131406ed797bb1
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-29 13:19:56 +0000

    Created Pages.PayerInformation.ClaimInformation

---
+++ client_code/Pages/PayerInformation/ClaimInformation/__init__.py
@@ -0,0 +1,63 @@
+from ._anvil_designer import ClaimInformationTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+"""
+    A form meant to be embedded used to fill out the roster information of the payers on the payer screen tab
+
+    Attributes
+    ----------
+    rosterDownloaded : dict
+        Holds information about the last file downloaded
+    rosterIngested : dict
+        Hold information about last file
+
+    Methods
+    -------
+    area() : float
+        Calculates the area of the rectangle
+    perimeter() : float
+        Calculates the perimeter of the rectangle
+    """
+class ClaimInformation(ClaimInformationTemplate):
+  def __init__(self, rosterDownloaded=None, rosterIngested=None, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.downloaded_files_grid.visible = False
+    '''
+    #self.item is bounded to the UI elements
+    self.item['rosterLastDownloadedDate'] = rosterDownloaded['Date']
+    self.item['rosterLastDownloadedName'] = rosterDownloaded['Name']
+    self.item['rosterLastDownloadedSize'] = rosterDownloaded['Size']
+
+    self.item['rosterLastDownloadedDate'] = rosterDownloaded['Date']
+    self.item['rosterLastDownloadedName'] = rosterDownloaded['Name']
+    self.item['rosterLastDownloadedSize'] = rosterDownloaded['Size']
+
+
+    self.item['rosterLastIngested'] = rosterIngested['']
+    self.item['']
+    '''
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.rich_text_1.visible = False
+
+  def show_downloaded_files_drop_down(self):
+    pass
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.downloaded_files_grid.visible:
+      self.downloaded_files_grid.visible = False
+
+    else:
+      self.downloaded_files_grid.visible = True
+
+  def radio_button_1_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    self.downloaded_files_grid.visible = not self.downloaded_files_grid.visible
