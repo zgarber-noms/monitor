7740cc0f86914c7c2c2d549c6e8cad1d3c141222
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-05-30 20:27:38 +0000

    Created Components.Icons.CheckMark and Components.Icons.RedX; Edited settings and Pages.RosterSummary

--- client_code/Pages/RosterSummary/__init__.py
+++ client_code/Pages/RosterSummary/__init__.py
@@ -4,10 +4,14 @@ import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
+from ...Components.Icons.CheckMark import CheckMark
+from ...Components.Icons.RedX import RedX
+
+
 
 class RosterSummary(RosterSummaryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.ACOReach_Symbol_Container.add_component(CheckMark())
     # Any code you write here will run before the form opens.
