519e690cd6def8f06c0dbeb81ba6495cb68fc6d2
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-06-05 18:16:21 +0000

    Created Components.Icons.Hourglass; Edited Pages.RosterSummary

--- client_code/Pages/RosterSummary/__init__.py
+++ client_code/Pages/RosterSummary/__init__.py
@@ -6,12 +6,16 @@ from anvil.google.drive import app_files
 import anvil.users
 from ...Components.Icons.CheckMark import CheckMark
 from ...Components.Icons.RedX import RedX
-
+from ...Components.Icons.Hourglass import Hourglass
 
 
 class RosterSummary(RosterSummaryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.ACOReach_Symbol_Container.add_component(CheckMark())
+    self.ACOReach_Symbol_Container.add_component(Hourglass())
+    self.MMOH_Symbol_Conatiner.add_component(Hourglass())
+    self.AnthemCommercial_Symbol_Conatiner.add_component((Hourglass()))
+    self.Devoted_Symbol_Conatiner.add_component(CheckMark())
+    self.Aetna_Symbol_Container.add_component(CheckMark())
     # Any code you write here will run before the form opens.
