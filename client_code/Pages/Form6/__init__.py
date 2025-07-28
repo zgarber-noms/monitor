9686fd980247a3c66cda56cc4f932bc3e52de4c3
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-07-28 14:35:38 +0000

    Created Pages.Form6

---
+++ client_code/Pages/Form6/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import Form6Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+
+class Form6(Form6Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
