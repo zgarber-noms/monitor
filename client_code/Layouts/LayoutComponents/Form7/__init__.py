057ad39cc06c7f62830128f71a17d7a5813fc088
zgarber@nomshealthcare.com <zgarber@nomshealthcare.com>
2025-07-28 16:47:21 +0000

    Created Layouts.LayoutComponents.Form7; Edited Pages.EmptyForm

---
+++ client_code/Layouts/LayoutComponents/Form7/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import Form7Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+
+class Form7(Form7Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
